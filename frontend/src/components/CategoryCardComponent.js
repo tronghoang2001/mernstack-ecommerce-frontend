6a9bc1d057199f4dfb0f6a0b06430d665357afa8
minhhoan1010 <lenguyenminhhoan@gmail.com>
2023-03-12 16:41:31 +0700

    frontend-second-commit

---
+++ frontend/src/components/CategoryCardComponent.js
@@ -0,0 +1,34 @@
+import { Card, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const CategoryCardComponent = ({ category, idx }) => {
+  const images = [
+    "/images/category/category_1.png",
+    "/images/category/category_2.jpg",
+    "/images/category/category_3.png",
+    "/images/category/category_4.png",
+    "/images/category/category_5.png",
+    "/images/category/category_6.jpg",
+    "/images/category/category_7.jpg",
+    "/images/category/category_8.png",
+    "/images/category/category_9.jpg",
+  ];
+  return (
+    <Card>
+      <Card.Img crossorigin="anonymous" variant="top" src={images[idx]} />
+      <Card.Body>
+        <Card.Title>{category}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <LinkContainer to="/product-list">
+          <Button variant="primary">Go to the Category</Button>
+        </LinkContainer>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CategoryCardComponent;
+
