7cc25bd084a5f1de15f9a00ffa58f7bd4a8c2e39
minhhoan1010 <lenguyenminhhoan@gmail.com>
2023-03-18 19:26:04 +0700

    frontend-commit-5

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -11,45 +11,87 @@ import {
 import { Rating } from "react-simple-star-rating";
 import AddedToCartMessageComponent from "../components/AddedToCartMessageComponent";
 
+import ImageZoom from "js-image-zoom";
+import { useEffect } from "react";
+
 const ProductDetailsPage = () => {
+  var options = {
+    // width: 400,
+    // zoomWidth: 500,
+    // fillContainer: true,
+    // zoomPosition: "bottom",
+    scale: 2,
+    offset: { vertical: 0, horizontal: 0 },
+  };
+  useEffect(() => {
+    new ImageZoom(document.getElementById("first"), options);
+    new ImageZoom(document.getElementById("second"), options);
+    new ImageZoom(document.getElementById("third"), options);
+    new ImageZoom(document.getElementById("fourth"), options);
+  });
   return (
     <Container>
       <AddedToCartMessageComponent />
       <Row className="mt-5">
-        <Col md={4}>
-          <Image fluid src="/images/games-category.png" />
-          <Image fluid src="/images/monitors-category.png" />
-          <Image fluid src="/images/tablets-category.png" />
-          <Image fluid src="/images/games-category.png" />
+        <Col style={{ zIndex: 1 }} md={4}>
+          <div id="first">
+            <Image
+              crossOrigin="anonymous"
+              fluid
+              src="/images/games-category.png"
+            />
+          </div>
+          <br />
+          <div id="second">
+            <Image fluid src="/images/monitors-category.png" />
+          </div>
+          <br />
+          <div id="third">
+            <Image fluid src="/images/tablets-category.png" />
+          </div>
+          <br />
+          <div id="fourth">
+            <Image fluid src="/images/games-category.png" />
+          </div>
+          <br />
         </Col>
         <Col md={8}>
           <Row>
             <Col md={8}>
               <ListGroup variant="flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>
+                  <h1>Product name</h1>
+                </ListGroup.Item>
                 <ListGroup.Item>
                   <Rating readonly size={20} initialValue={4} /> (1)
                 </ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                <ListGroup.Item>
+                  Price <span className="fw-bold">$345</span>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Porta ac consectetur ac Lorem ipsum dolor, sit amet
+                  consectetur adipisicing elit. Perferendis, illo.
+                </ListGroup.Item>
               </ListGroup>
             </Col>
             <Col md={4}>
               <ListGroup>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Status: in stock</ListGroup.Item>
                 <ListGroup.Item>
+                  Price: <span className="fw-bold">$345</span>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Quantity:
                   <Form.Select size="lg" aria-label="Default select example">
-                    <option>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <option>1</option>
+                    <option value="1">2</option>
+                    <option value="2">3</option>
+                    <option value="3">4</option>
                   </Form.Select>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <Button variant="danger">Danger</Button>
+                  <Button variant="danger">Add to cart</Button>
                 </ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
               </ListGroup>
             </Col>
           </Row>
@@ -57,10 +99,16 @@ const ProductDetailsPage = () => {
             <Col className="mt-5">
               <h5>REVIEWS</h5>
               <ListGroup variant="flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                {Array.from({ length: 10 }).map((item, idx) => (
+                  <ListGroup.Item key={idx}>
+                    John Doe <br />
+                    <Rating readonly size={20} initialValue={4} />
+                    <br />
+                    20-09-2001 <br />
+                    Porta ac consectetur ac Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Perferendis, illo.
+                  </ListGroup.Item>
+                ))}
               </ListGroup>
             </Col>
           </Row>
@@ -68,23 +116,20 @@ const ProductDetailsPage = () => {
           <Alert variant="danger">Login first to write a review</Alert>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
+              <Form.Label>Write a review</Form.Label>
               <Form.Control as="textarea" rows={3} />
             </Form.Group>
             <Form.Select aria-label="Default select example">
-              <option>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+              <option>Your rating</option>
+              <option value="5">5 (very good)</option>
+              <option value="4">4 (good)</option>
+              <option value="3">3 (average)</option>
+              <option value="2">2 (bad)</option>
+              <option value="1">1 (awful)</option>
             </Form.Select>
-            <Button variant="primary">Primary</Button>
+            <Button className="mb-3 mt-3" variant="primary">
+              Submit
+            </Button>
           </Form>
         </Col>
       </Row>
